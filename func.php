81c8e3e2642c750bc319e3f9b7b0a9b00f9c7bd7
akbardwi <akbar.dwi14@gmail.com>
2020-03-04 07:22:47 +0700

    Update func.php

--- func.php
+++ func.php
@@ -239,7 +239,7 @@ function claims($token,$voc)
 
 function reff($token)
     {
-    $data = '{"referral_code":"G-75SR565"}';    
+    $data = '{"referral_code":"G-WDMK8ZH"}';    
     $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
     if ($claim['success'] == 1)
         {
