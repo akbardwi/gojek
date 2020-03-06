197db5f834f48669d4208ddf213128c2096ee0bd
akbardwi <akbar.dwi14@gmail.com>
2020-03-06 20:18:27 +0700

    Update func.php

--- func.php
+++ func.php
@@ -78,6 +78,21 @@ function register($no)
         }
     }
 
+function color($color = "default" , $text)
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
+
     function login($no)
     {
 
